4cf22c2fdb990a3f2e4438aa63c3c81e11dc29a2
gabriel <munteanugabriel25@yahoo.com>
2021-08-03 11:21:09 +0300

    add controller.js and model.js

--- frontend/src/js/model.js
+++ frontend/src/js/model.js
@@ -1,20 +1,38 @@
 import {getJSON} from "./helpers.js"
 import {URL_API} from "./config.js"
-
+import {RESULTS_PER_PAGE} from "./config.js"
 
 export const state = {
-    item : {},
+    item :{},
+    quantity:{},
+    search :{
+        query:"",
+        results: [],
+        resultsPerPage:RESULTS_PER_PAGE,
+        pageNumber: 1
+    },
 }
 
 
-
-export const loadItem = async function(id) {
+export const loadItem =  async function(id){
     try {
-        const data = await getJSON(`${URL_API}/${id}`)
-        console.log(data)
-        
+        const data = await getJSON(`${URL_API}/${id}`);
+        state.item = data;
+    }catch(error){
+        console.error(error)
     }
-    catch(error){
+}
+
+
+export const loadSearchResults = async function(query){
+    try {
+        data = await getJSON(`${URL_API}/?search=${query}`);
+        console.log(data);
+        state.search.query=query;
+        state.search.results=data;
+        state.search.pageNumber=1;
+
+    }catch(error){
         console.error(error)
     }
 }
